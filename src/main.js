0d8186575b643eae816001cf32b710490152d04a
amir <amirtaki623@gmail.com>
2025-08-30 22:18:56 +0330

    part 35

--- src/main.js
+++ src/main.js
@@ -1,3 +1,9 @@
 const header = document.querySelector("header")
 const menuBtn = document.querySelector("#menu-btn")
 const closeMenuBtn = document.querySelector("#close-menu-btn")
+
+
+
+menuBtn.addEventListener("click", (e) => {
+    header.classList.toggle("show-mobile-menu")
+})
